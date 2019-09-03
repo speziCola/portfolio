2446e518e3edb39d02b0a1a78abab355390378f5
speziCola <chris@qrt.de>
2019-09-04 00:39:49 +0200

    images

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#4264fb`,
         theme_color: `#4264fb`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ck-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
