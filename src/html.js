48111c648d6ff9bbc5da5492f973e36f84fe5f10
speziCola <chris@qrt.de>
2020-04-15 13:42:11 +0200

    cache

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0"
         />
+        <meta http-equiv="Cache-control" content="public"></meta>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
